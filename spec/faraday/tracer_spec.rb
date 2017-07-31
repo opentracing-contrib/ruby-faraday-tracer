6df86ac090aeb75a67d9eb8e37154097b0f84e68
iaintshine <bodziomista@gmail.com>
2017-07-31 21:15:07 +0200

    Allow to pass Proc as parent span provider

--- spec/faraday/tracer_spec.rb
+++ spec/faraday/tracer_spec.rb
@@ -16,11 +16,28 @@ RSpec.describe Faraday::Tracer do
     expect(span.tags['span.kind']).to eq('client')
   end
 
+  describe 'parent_span' do
+    it 'allows to pass a pre-created parent span' do
+      parent_span = tracer.start_span("parent_span")
+      expect(tracer).to receive(:start_span).with(any_args, hash_including(child_of: parent_span)).and_call_original
+      call(method: :post, span: parent_span)
+    end
+
+    it 'allows to pass a block as a parent span provider' do
+      parent_span = tracer.start_span("parent_span")
+      parent_span_provider = lambda { parent_span }
+
+      expect(tracer).to receive(:start_span).with(any_args, hash_including(child_of: parent_span)).and_call_original
+      call(method: :post, span: parent_span_provider)
+    end
+  end
+
   def call(options)
+    span = options.delete(:span)
     app = lambda {|env| env}
     env = Faraday::Env.from(options)
     allow(env).to receive(:on_complete).and_yield(double(status: 200))
-    middleware = described_class.new(app, tracer: tracer)
+    middleware = described_class.new(app, span: span, tracer: tracer)
     middleware.call(env)
   end
 end
